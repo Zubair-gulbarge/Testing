058251c696b9aa1d74c6a2648e034ce6b1bb26d0
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-10-19 18:22:20 +0530

    Delete form.py

--- form.py
+++
@@ -1,7 +0,0 @@
-# Collect user information
-name = input("Enter your name: ")
-email = input("Enter your email address: ")
-
-# Display collected information
-print("Name: " + name)
-print("Email: " + email)
