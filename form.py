95cc5587a45b4dbafc3cb4692938ce0facf6c483
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-10-19 17:59:33 +0530

    Update form.py

---
+++ form.py
@@ -0,0 +1,7 @@
+# Collect user information
+name = input("Enter your name: ")
+email = input("Enter your email address: ")
+
+# Display collected information
+print("Name: " + name)
+print("Email: " + email)
