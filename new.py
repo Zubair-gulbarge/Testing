ea555f9feaa48fbcd3e9ddb005c55972b96cbb90
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-10-21 01:56:44 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,3 @@
-response = input("Enter a number (1 or 2): ")
-
-if response == "1":
-    print("I am case 1")
-elif response == "2":
-    print("I am case 2")
-else:
-    print("Invalid input. Please enter 1 or 2.")
+for i in range(3):
+    for j in range(2):
+        print(f"({i}, {j})")
