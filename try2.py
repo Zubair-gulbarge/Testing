6771e0adfbc0399961b04e7c8d59f0f7ee48812f
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-10-26 22:03:40 +0530

    Update try2.py

--- try2.py
+++ try2.py
@@ -1,13 +1,28 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# # Test the function
+# number = int(input("Enter a non-negative integer: "))
+# if number < 0:
+#     print("Factorial is not defined for negative numbers.")
+# else:
+#     result = factorial(number)
+#     print(f"The factorial of {number} is {result}")
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
 # Test the function
-number = int(input("Enter a non-negative integer: "))
-if number < 0:
-    print("Factorial is not defined for negative numbers.")
+num1 = int(input("Enter the first positive integer: "))
+num2 = int(input("Enter the second positive integer: "))
+
+if num1 <= 0 or num2 <= 0:
+    print("Both numbers should be positive integers.")
 else:
-    result = factorial(number)
-    print(f"The factorial of {number} is {result}")
+    result = gcd(num1, num2)
+    print(f"The GCD of {num1} and {num2} is {result}")
