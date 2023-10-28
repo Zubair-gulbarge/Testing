85e364c03574a5c229efa790db7fe44b1ddcb686
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-10-28 18:09:07 +0530

    Update try1.py

--- try1.py
+++ try1.py
@@ -19,3 +19,18 @@
 #     print("You are overweight.")
 # else:
 #     print("You are obese.")
+
+def is_palindrome(string):
+    # Remove spaces and convert to lowercase for case-insensitive comparison
+    cleaned_string = string.replace(" ", "").lower()
+    # Compare the original string with its reverse
+    return cleaned_string == cleaned_string[::-1]
+
+# Test the function
+input_string = input("Enter a string: ")
+
+if is_palindrome(input_string):
+    print("It's a palindrome.")
+else:
+    print("It's not a palindrome.")
+
