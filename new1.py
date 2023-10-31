b386fe5376dc04207cad1116d6361f0828cc2d7d
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-10-31 22:24:41 +0530

    Update new1.py

--- new1.py
+++ new1.py
@@ -1,11 +1,11 @@
-import math
+# import math
 
-# Get the radius from the user
-radius = float(input("Enter the radius of the circle: "))
+# # Get the radius from the user
+# radius = float(input("Enter the radius of the circle: "))
 
-# Calculate the area of the circle
-area = math.pi * (radius ** 2)
+# # Calculate the area of the circle
+# area = math.pi * (radius ** 2)
 
-# Print the result
-print(f"The area of the circle with radius {radius} is {area:.2f}")
+# # Print the result
+# print(f"The area of the circle with radius {radius} is {area:.2f}")
 
