1b7be7c2d0776fd781bcee655dbd11ff82e7f631
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-01 23:57:28 +0530

    Update new2.py

--- new2.py
+++ new2.py
@@ -1,18 +1,18 @@
-# Find the factors of a positive integer
-number = int(input("Enter a positive integer: "))
+# # Find the factors of a positive integer
+# number = int(input("Enter a positive integer: "))
 
-factors = []
+# factors = []
 
-for i in range(1, number + 1):
-    if number % i == 0:
-        factors.append(i)
+# for i in range(1, number + 1):
+#     if number % i == 0:
+#         factors.append(i)
 
-print(f"The factors of {number} are: {factors}")
+# print(f"The factors of {number} are: {factors}")
 
 
-# Find the largest element in a list of numbers
-numbers = [12, 7, 15, 28, 21]
+# # Find the largest element in a list of numbers
+# numbers = [12, 7, 15, 28, 21]
 
-largest = max(numbers)
+# largest = max(numbers)
 
-print(f"The largest element in the list is: {largest}")
+# print(f"The largest element in the list is: {largest}")
