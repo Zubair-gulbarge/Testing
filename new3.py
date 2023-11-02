9731b3cc63ef3f958f7cc798911573b848f5bc76
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-02 15:32:23 +0530

    Update new3.py

--- new3.py
+++ new3.py
@@ -1,6 +1,26 @@
-# Reverse a list
-original_list = [1, 2, 3, 4, 5]
+# # Reverse a list
+# original_list = [1, 2, 3, 4, 5]
 
-reversed_list = original_list[::-1]
+# reversed_list = original_list[::-1]
 
-print(f"The reversed list is: {reversed_list}")
+# print(f"The reversed list is: {reversed_list}")
+
+# Check if a number is an Armstrong number
+number = int(input("Enter a positive integer: "))
+
+def is_armstrong(num):
+    order = len(str(num))
+    total = 0
+    temp = num
+
+    while temp > 0:
+        digit = temp % 10
+        total += digit ** order
+        temp //= 10
+
+    return num == total
+
+if is_armstrong(number):
+    print(f"{number} is an Armstrong number.")
+else:
+    print(f"{number} is not an Armstrong number.")
