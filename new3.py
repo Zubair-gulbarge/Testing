72237668f130a5590d7f0fc23b30d6b162997edb
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-02 15:33:51 +0530

    Update new3.py

--- new3.py
+++ new3.py
@@ -9,12 +9,12 @@
 # number = int(input("Enter a positive integer: "))
 
 # Calculate the power of a number
-base = int(input("Enter the base: "))
-exponent = int(input("Enter the exponent: "))
+# base = int(input("Enter the base: "))
+# exponent = int(input("Enter the exponent: "))
 
-result = base ** exponent
+# result = base ** exponent
 
-print(f"{base} raised to the power of {exponent} is {result}")
+# print(f"{base} raised to the power of {exponent} is {result}")
 
 
 # def is_armstrong(num):
