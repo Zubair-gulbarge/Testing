92930fbc92844fcaeda6e77740c4637ad7db7a1d
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-04 19:27:22 +0530

    Update new4.py

--- new4.py
+++ new4.py
@@ -16,3 +16,36 @@
 # target = 7
 # result = binary_search(sorted_list, target)
 # print(f"Index of {target} is {result}")
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Example usage:
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.display()
