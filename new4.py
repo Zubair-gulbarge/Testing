4bdffb11fee47b676d630a2c1776b705c8588c1d
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-05 15:33:21 +0530

    Update new4.py

--- new4.py
+++ new4.py
@@ -80,3 +80,44 @@
 # string2 = "GXTXAYB"
 # result = longest_common_subsequence(string1, string2)
 # print(f"Longest Common Subsequence: {result}")
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+class Queue:
+    def __init(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+# Example usage:
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+print(queue.dequeue())
