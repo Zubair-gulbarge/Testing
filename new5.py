7c19ac2648b8e39b6cc1a2be57c7c3b665639f4a
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-06 23:44:17 +0530

    Update new5.py

--- new5.py
+++ new5.py
@@ -22,3 +22,27 @@
 # quotes = soup.find_all("span", class_="text")
 # for quote in quotes:
 #     print(quote.get_text())
+
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: ")
+
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Cannot divide by zero"
+    else:
+        result = "Invalid operator"
+
+    print(f"Result: {result}")
+
+# Example usage:
+calculator()
