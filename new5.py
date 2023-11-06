afc1f1ad3125be00393385ace1fa8c2b0c6a81c7
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-06 23:40:48 +0530

    Update new5.py

---
+++ new5.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def group_anagrams(words):
+    anagrams = defaultdict(list)
+    for word in words:
+        sorted_word = "".join(sorted(word))
+        anagrams[sorted_word].append(word)
+    return list(anagrams.values())
+
+# Example usage:
+word_list = ["listen", "silent", "hello", "world", "evil", "vile"]
+anagram_groups = group_anagrams(word_list)
+print("Anagram Groups:", anagram_groups)
