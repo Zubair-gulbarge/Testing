25d1ce6c42a930e8f20fb6a0b1c9a0dbcb3084b1
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-06 23:46:25 +0530

    Update new5.py

--- new5.py
+++ new5.py
@@ -23,26 +23,26 @@
 # for quote in quotes:
 #     print(quote.get_text())
 
-def calculator():
-    num1 = float(input("Enter the first number: "))
-    operator = input("Enter an operator (+, -, *, /): ")
-    num2 = float(input("Enter the second number: ")
+# def calculator():
+#     num1 = float(input("Enter the first number: "))
+#     operator = input("Enter an operator (+, -, *, /): ")
+#     num2 = float(input("Enter the second number: ")
 
-    if operator == "+":
-        result = num1 + num2
-    elif operator == "-":
-        result = num1 - num2
-    elif operator == "*":
-        result = num1 * num2
-    elif operator == "/":
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            result = "Cannot divide by zero"
-    else:
-        result = "Invalid operator"
+#     if operator == "+":
+#         result = num1 + num2
+#     elif operator == "-":
+#         result = num1 - num2
+#     elif operator == "*":
+#         result = num1 * num2
+#     elif operator == "/":
+#         if num2 != 0:
+#             result = num1 / num2
+#         else:
+#             result = "Cannot divide by zero"
+#     else:
+#         result = "Invalid operator"
 
-    print(f"Result: {result}")
+#     print(f"Result: {result}")
 
-# Example usage:
-calculator()
+# # Example usage:
+# calculator()
