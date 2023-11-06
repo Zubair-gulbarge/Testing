ca546a6443867ba77e7cbd8e48eb3b7d14003201
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-06 23:43:14 +0530

    Update new5.py

--- new5.py
+++ new5.py
@@ -11,3 +11,14 @@
 # word_list = ["listen", "silent", "hello", "world", "evil", "vile"]
 # anagram_groups = group_anagrams(word_list)
 # print("Anagram Groups:", anagram_groups)
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+quotes = soup.find_all("span", class_="text")
+for quote in quotes:
+    print(quote.get_text())
