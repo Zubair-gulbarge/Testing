fde419752599db80c394eb0beceb1b99bea9ed00
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-07 22:37:14 +0530

    Update new6.py

--- new6.py
+++ new6.py
@@ -34,3 +34,34 @@
 # my_todo_list.list_tasks()
 # my_todo_list.remove_task("Buy groceries")
 # my_todo_list.list_tasks()
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        else:
+            return "Invalid traversal type"
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.data) + " ")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+# Example usage:
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+print("Preorder traversal:", tree.print_tree("preorder"))
