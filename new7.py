20bad5a958e67a182e0f016dd9b0c6f07701c971
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-08 13:02:39 +0530

    Update new7.py

--- new7.py
+++ new7.py
@@ -8,3 +8,18 @@
 # input_list = [1, 2, -8]
 # result = sum_of_list_elements(input_list)
 # print("Sum of elements:", result)
+
+def sum_of_list_elements(input_list):
+    total = 0
+    for element in input_list:
+        total += element
+    return total
+
+# Take input from the user as a comma-separated list of numbers
+user_input = input("Enter a list of numbers separated by commas: ")
+
+# Split the input string into a list of numbers
+input_list = [int(x) for x in user_input.split(",")]
+
+result = sum_of_list_elements(input_list)
+print("Sum of elements:", result)
