d68b01f51b9ec68727d178845f1e57c753bf3c58
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-09 20:02:31 +0530

    Update new8.py

---
+++ new8.py
@@ -0,0 +1,12 @@
+# Take the input string from the user
+s = input("Enter a string: ")
+
+# Take the number of characters to display from the user
+n = int(input("Enter the number of characters from the right: "))
+
+# Display the last n characters from the right of the string
+if n >= 0:
+    result = s[-n:]
+    print(f"The last {n} characters from the right: {result}")
+else:
+    print("Invalid input for the number of characters.")
