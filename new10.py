fde0c5bbb077cf10f587912483e0149c39f3dd5d
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-11 15:13:45 +0530

    Update new10.py

--- new10.py
+++ new10.py
@@ -23,3 +23,14 @@
 #     print(f"{number} is a prime number.")
 # else:
 #     print(f"{number} is not a prime number.")
+
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+# Example usage:
+n = 8
+result = fibonacci(n)
+print(f"The first {n} numbers in the Fibonacci series are: {result}")
