229a0861fd66fb1305652f7ac976d9ff2864e17d
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-11 15:13:14 +0530

    Update new10.py

--- new10.py
+++ new10.py
@@ -9,17 +9,17 @@
 # result = factorial(n)
 # print(f"The factorial of {n} is: {result}")
 
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+# def is_prime(num):
+#     if num < 2:
+#         return False
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
 
-# Example usage:
-number = 13
-if is_prime(number):
-    print(f"{number} is a prime number.")
-else:
-    print(f"{number} is not a prime number.")
+# # Example usage:
+# number = 13
+# if is_prime(number):
+#     print(f"{number} is a prime number.")
+# else:
+#     print(f"{number} is not a prime number.")
