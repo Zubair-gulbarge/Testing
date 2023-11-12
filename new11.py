98ce0978b26b7dbfbb110ef32035a173f1664dbd
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-12 22:42:27 +0530

    Update new11.py

--- new11.py
+++ new11.py
@@ -7,9 +7,9 @@
 # else:
 #     print("Zero")
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# num3 = float(input("Enter the third number: "))
 
-max_num = max(num1, num2, num3)
-print(f"The maximum number is: {max_num}")
+# max_num = max(num1, num2, num3)
+# print(f"The maximum number is: {max_num}")
