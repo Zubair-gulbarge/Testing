02e43ecbac0535f0c53d4932dcbf2fe4ff2ba3d4
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-12 22:41:48 +0530

    Update new11.py

--- new11.py
+++ new11.py
@@ -6,3 +6,10 @@
 #     print("Negative number")
 # else:
 #     print("Zero")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+max_num = max(num1, num2, num3)
+print(f"The maximum number is: {max_num}")
