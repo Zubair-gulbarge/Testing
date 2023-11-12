a873d57fdd5d54f5578154257cd6286bb6c7d124
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-12 22:42:57 +0530

    Update new11.py

--- new11.py
+++ new11.py
@@ -13,3 +13,10 @@
 
 # max_num = max(num1, num2, num3)
 # print(f"The maximum number is: {max_num}")
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
