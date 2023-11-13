6b4561470a33235ebdd42b86ab964cbf919d9f27
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-13 21:02:18 +0530

    Update new11.py

--- new11.py
+++ new11.py
@@ -32,14 +32,14 @@
 # else:
 #     print("Scalene triangle")
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num > 1:
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            print("Not a prime number")
-            break
-    else:
-        print("Prime number")
-else:
-    print("Not a prime number")
+# if num > 1:
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             print("Not a prime number")
+#             break
+#     else:
+#         print("Prime number")
+# else:
+#     print("Not a prime number")
