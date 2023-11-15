19344069c1e4d904e2c7c109bb8472613e88e1cd
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-15 16:16:57 +0530

    Update new12.py

--- new12.py
+++ new12.py
@@ -34,19 +34,19 @@
 
 #     return x == reversed_num or x == reversed_num // 10
 
-def roman_to_int(s):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    prev_value = 0
+# def roman_to_int(s):
+#     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#     result = 0
+#     prev_value = 0
 
-    for char in reversed(s):
-        current_value = roman_dict[char]
+#     for char in reversed(s):
+#         current_value = roman_dict[char]
 
-        if current_value >= prev_value:
-            result += current_value
-        else:
-            result -= current_value
+#         if current_value >= prev_value:
+#             result += current_value
+#         else:
+#             result -= current_value
 
-        prev_value = current_value
+#         prev_value = current_value
 
-    return result
+#     return result
