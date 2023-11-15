9d42932af956dd6f826b4cbfc45d829ed8ff971e
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-15 16:16:07 +0530

    Update new12.py

--- new12.py
+++ new12.py
@@ -23,13 +23,13 @@
 #         result = result * 10 + pop
 
 #     return sign * result
-def is_palindrome(x):
-    if x < 0 or (x % 10 == 0 and x != 0):
-        return False
+# def is_palindrome(x):
+#     if x < 0 or (x % 10 == 0 and x != 0):
+#         return False
 
-    reversed_num = 0
-    while x > reversed_num:
-        reversed_num = reversed_num * 10 + x % 10
-        x //= 10
+#     reversed_num = 0
+#     while x > reversed_num:
+#         reversed_num = reversed_num * 10 + x % 10
+#         x //= 10
 
-    return x == reversed_num or x == reversed_num // 10
+#     return x == reversed_num or x == reversed_num // 10
