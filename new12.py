de2d5610268fefacebb4f19e8547902510ce584f
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-15 16:14:12 +0530

    Update new12.py

--- new12.py
+++ new12.py
@@ -1,8 +1,8 @@
-def two_sum(nums, target):
-    complement_dict = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in complement_dict:
-            return [complement_dict[complement], i]
-        complement_dict[num] = i
-    return None
+# def two_sum(nums, target):
+#     complement_dict = {}
+#     for i, num in enumerate(nums):
+#         complement = target - num
+#         if complement in complement_dict:
+#             return [complement_dict[complement], i]
+#         complement_dict[num] = i
+#     return None
