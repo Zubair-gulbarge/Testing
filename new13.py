66f5dc74a8f93d9297d65ff1b6a8f8313e4e80a6
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-17 02:01:39 +0530

    Update new13.py

--- new13.py
+++ new13.py
@@ -12,3 +12,26 @@
 # # Accessing attributes and calling a method
 # print(f"{my_dog.name} is {my_dog.age} years old.")
 # my_dog.bark()
+
+class Animal:
+    def __init__(self, species):
+        self.species = species
+
+    def make_sound(self):
+        pass
+
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow!")
+
+class Dog(Animal):
+    def make_sound(self):
+        print("Woof!")
+
+# Creating objects of the derived classes
+my_cat = Cat("Feline")
+my_dog = Dog("Canine")
+
+# Calling the overridden method
+my_cat.make_sound()
+my_dog.make_sound()
