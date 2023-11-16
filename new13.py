6ac43ee4c1377c2116111d9981329d4266e343aa
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-17 01:59:18 +0530

    Update new13.py

--- new13.py
+++ new13.py
@@ -1,14 +1,14 @@
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def bark(self):
-        print(f"{self.name} says Woof!")
+#     def bark(self):
+#         print(f"{self.name} says Woof!")
 
-# Creating an object of the Dog class
-my_dog = Dog("Buddy", 3)
+# # Creating an object of the Dog class
+# my_dog = Dog("Buddy", 3)
 
-# Accessing attributes and calling a method
-print(f"{my_dog.name} is {my_dog.age} years old.")
-my_dog.bark()
+# # Accessing attributes and calling a method
+# print(f"{my_dog.name} is {my_dog.age} years old.")
+# my_dog.bark()
