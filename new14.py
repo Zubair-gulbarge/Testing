d29c9fd4ecf8653448841f1d4f9ad6345e180e6e
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-18 11:48:31 +0530

    Update new14.py

--- new14.py
+++ new14.py
@@ -1,26 +1,26 @@
-class Shape:
-    def area(self):
-        pass
+# class Shape:
+#     def area(self):
+#         pass
 
-class Rectangle(Shape):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+# class Rectangle(Shape):
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
 
-    def area(self):
-        return self.length * self.width
+#     def area(self):
+#         return self.length * self.width
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
 
-    def area(self):
-        return 3.14 * self.radius ** 2
+#     def area(self):
+#         return 3.14 * self.radius ** 2
 
-# Creating objects of the derived classes
-rectangle = Rectangle(5, 8)
-circle = Circle(4)
+# # Creating objects of the derived classes
+# rectangle = Rectangle(5, 8)
+# circle = Circle(4)
 
-# Calculating and displaying areas
-print(f"Area of the rectangle: {rectangle.area()}")
-print(f"Area of the circle: {circle.area()}")
+# # Calculating and displaying areas
+# print(f"Area of the rectangle: {rectangle.area()}")
+# print(f"Area of the circle: {circle.area()}")
