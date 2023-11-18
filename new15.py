b2b3a0e44285ab88b560e5fe70027f408ae2cd56
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-19 00:36:33 +0530

    Update new15.py

--- new15.py
+++ new15.py
@@ -1,6 +1,6 @@
 # Printing numbers from 1 to 5 using a for loop
-for i in range(1, 6):
-    print(i)
+# for i in range(1, 6):
+#     print(i)
 
 # Countdown from 5 to 1 using a while loop
 # count = 5
