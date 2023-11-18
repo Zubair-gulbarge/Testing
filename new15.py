46f13f4a593dedad054d583ea32b39cc88b4f9cc
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-19 00:36:52 +0530

    Update new15.py

--- new15.py
+++ new15.py
@@ -7,3 +7,9 @@
 # while count > 0:
 #     print(count)
 #     count -= 1
+
+# Calculating the sum of numbers from 1 to 5 using a for loop
+sum_numbers = 0
+for i in range(1, 6):
+    sum_numbers += i
+print("Sum:", sum_numbers)
