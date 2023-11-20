475439b5cd54115f00cd80171b0a8cc001e6f32a
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-20 13:53:47 +0530

    Update new16.py

--- new16.py
+++ new16.py
@@ -5,3 +5,9 @@
 #     factorial *= num
 #     num -= 1
 # print("Factorial:", factorial)
+
+# Printing a pattern using nested for loops
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end=" ")
+    print()
