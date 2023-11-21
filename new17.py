ed2f4acfed32ea44343e37554821fe33b4cc4b7a
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-21 17:00:56 +0530

    Update new17.py

--- new17.py
+++ new17.py
@@ -1,14 +1,14 @@
-# Simple number guessing game using a while loop
-import random
+# # Simple number guessing game using a while loop
+# import random
 
-target_number = random.randint(1, 20)
-guess = 0
+# target_number = random.randint(1, 20)
+# guess = 0
 
-while guess != target_number:
-    guess = int(input("Guess the number (between 1 and 20): "))
-    if guess < target_number:
-        print("Too low. Try again.")
-    elif guess > target_number:
-        print("Too high. Try again.")
+# while guess != target_number:
+#     guess = int(input("Guess the number (between 1 and 20): "))
+#     if guess < target_number:
+#         print("Too low. Try again.")
+#     elif guess > target_number:
+#         print("Too high. Try again.")
 
-print("Congratulations! You guessed the correct number:", target_number)
+# print("Congratulations! You guessed the correct number:", target_number)
