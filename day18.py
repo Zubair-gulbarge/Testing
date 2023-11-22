b9aef9a549cfca07f4d03b7dd13a971c34fc040b
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-22 18:58:38 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -1,3 +1,3 @@
-# Using list comprehension to create a new list
-squares = [x**2 for x in range(1, 6)]
-print("Squares:", squares)
+# # Using list comprehension to create a new list
+# squares = [x**2 for x in range(1, 6)]
+# print("Squares:", squares)
