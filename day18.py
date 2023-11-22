b919e0db26c02a13e19109a997711f33beb04ca9
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-22 18:59:00 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -1,3 +1,14 @@
 # # Using list comprehension to create a new list
 # squares = [x**2 for x in range(1, 6)]
 # print("Squares:", squares)
+
+# Calculating the sum of digits of a number using a while loop
+number = 12345
+sum_of_digits = 0
+
+while number > 0:
+    digit = number % 10
+    sum_of_digits += digit
+    number //= 10
+
+print("Sum of digits:", sum_of_digits)
