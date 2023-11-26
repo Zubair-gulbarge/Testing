243ae85746ee60c1b5ceff64deef12f36cb38572
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-26 23:23:37 +0530

    Update day19.py

--- day19.py
+++ day19.py
@@ -13,3 +13,14 @@
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # even_numbers = [num for num in numbers if num % 2 == 0]
 # print("Even Numbers:", even_numbers)
+
+# Reversing a number using a while loop
+# original_number = 12345
+# reversed_number = 0
+
+# while original_number > 0:
+#     digit = original_number % 10
+#     reversed_number = reversed_number * 10 + digit
+#     original_number //= 10
+
+# print("Reversed Number:", reversed_number)
