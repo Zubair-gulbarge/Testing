43f27e4ae961ebeda626d0092d39c48c23c0cd17
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-26 23:22:43 +0530

    Update day19.py

--- day19.py
+++ day19.py
@@ -8,3 +8,8 @@
 #     exponent -= 1
 
 # print(f"{base} raised to the power of {exponent}: {result}")
+
+# Filtering even numbers from a list using a for loop
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = [num for num in numbers if num % 2 == 0]
+print("Even Numbers:", even_numbers)
