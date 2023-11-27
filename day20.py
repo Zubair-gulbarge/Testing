c6a7632498626cff0426f124647e1ea79dae217b
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-27 15:38:04 +0530

    Update day20.py

--- day20.py
+++ day20.py
@@ -19,15 +19,15 @@
 #     print(f"{word} is not a palindrome.")
 
 # Checking if a word is a palindrome using a while loop
-word = "radar"
-original_word = word.lower()
-reversed_word = ""
+# word = "radar"
+# original_word = word.lower()
+# reversed_word = ""
 
-while original_word:
-    reversed_word += original_word[-1]
-    original_word = original_word[:-1]
+# while original_word:
+#     reversed_word += original_word[-1]
+#     original_word = original_word[:-1]
 
-if word.lower() == reversed_word:
-    print(f"{word} is a palindrome.")
-else:
-    print(f"{word} is not a palindrome.")
+# if word.lower() == reversed_word:
+#     print(f"{word} is a palindrome.")
+# else:
+#     print(f"{word} is not a palindrome.")
