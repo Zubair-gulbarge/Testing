70cc5c65c51c3e251ff868c72bf5d6e452994f6b
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-27 15:36:28 +0530

    Update day20.py

--- day20.py
+++ day20.py
@@ -1,5 +1,5 @@
 # Printing a pattern using a nested for loop
-for i in range(5):
-    for j in range(i + 1):
-        print("*", end=" ")
-    print()
+# for i in range(5):
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print()
