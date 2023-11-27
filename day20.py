95024e731d19ae655b46a7149a8064a2aee9bab6
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-27 15:36:47 +0530

    Update day20.py

--- day20.py
+++ day20.py
@@ -3,3 +3,17 @@
 #     for j in range(i + 1):
 #         print("*", end=" ")
 #     print()
+
+# Checking if a word is a palindrome using a while loop
+word = "radar"
+original_word = word.lower()
+reversed_word = ""
+
+while original_word:
+    reversed_word += original_word[-1]
+    original_word = original_word[:-1]
+
+if word.lower() == reversed_word:
+    print(f"{word} is a palindrome.")
+else:
+    print(f"{word} is not a palindrome.")
