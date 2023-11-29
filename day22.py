99c995c10a50818772038c6473e310895c090723
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-30 00:31:18 +0530

    Update day22.py

--- day22.py
+++ day22.py
@@ -5,3 +5,14 @@
 
 # with open("result.txt", "w") as result_file:
 #     result_file.write(f"Sum of Numbers: {sum_of_numbers}")
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+headlines = soup.find_all("h2")
+for headline in headlines:
+    print(headline.text)
