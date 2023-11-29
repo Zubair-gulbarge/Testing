c6c59fb013820fa0c74cf46012f264b3e3272389
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-30 00:32:04 +0530

    Update day22.py

--- day22.py
+++ day22.py
@@ -16,3 +16,25 @@
 # headlines = soup.find_all("h2")
 # for headline in headlines:
 #     print(headline.text)
+
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect("example.db")
+cursor = conn.cursor()
+
+# Create a table
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
+
+# Insert data
+cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Alice", 25))
+cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Bob", 30))
+
+# Query the database
+cursor.execute("SELECT * FROM users")
+for row in cursor.fetchall():
+    print(row)
+
+# Close the connection
+conn.commit()
+conn.close()
