d992f740f6974254f062db238dba43c67be9395c
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-11-30 00:30:54 +0530

    Update day22.py

--- day22.py
+++ day22.py
@@ -1,7 +1,7 @@
-with open("numbers.txt", "r") as file:
-    numbers = [int(line.strip()) for line in file]
+# with open("numbers.txt", "r") as file:
+#     numbers = [int(line.strip()) for line in file]
 
-sum_of_numbers = sum(numbers)
+# sum_of_numbers = sum(numbers)
 
-with open("result.txt", "w") as result_file:
-    result_file.write(f"Sum of Numbers: {sum_of_numbers}")
+# with open("result.txt", "w") as result_file:
+#     result_file.write(f"Sum of Numbers: {sum_of_numbers}")
