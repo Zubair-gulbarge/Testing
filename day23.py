1ff138b26c90ddd42f9fbf3dda1b0b8c6c6a9639
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-01 01:24:11 +0530

    Update day23.py

--- day23.py
+++ day23.py
@@ -1,13 +1,13 @@
-import pandas as pd
+# import pandas as pd
 
-# Load dataset
-df = pd.read_csv("example_dataset.csv")
+# # Load dataset
+# df = pd.read_csv("example_dataset.csv")
 
-# Filter data
-filtered_data = df[df["column_name"] > 50]
+# # Filter data
+# filtered_data = df[df["column_name"] > 50]
 
-# Group by and aggregate
-grouped_data = df.groupby("category_column").agg({"numeric_column": "mean"})
+# # Group by and aggregate
+# grouped_data = df.groupby("category_column").agg({"numeric_column": "mean"})
 
-# Data visualization
-grouped_data.plot(kind="bar")
+# # Data visualization
+# grouped_data.plot(kind="bar")
