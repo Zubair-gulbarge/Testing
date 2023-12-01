d0ce52d451d5815680f81b0a1d8cf7b2d88a27a6
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-02 02:49:51 +0530

    Update day24.py

--- day24.py
+++ day24.py
@@ -15,3 +15,14 @@
 # sid = SentimentIntensityAnalyzer()
 # sentiment_score = sid.polarity_scores(text)
 # print("Sentiment Score:", sentiment_score)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="Home", content="Welcome to my Flask app!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
