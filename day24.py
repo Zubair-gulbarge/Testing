98ed30b1c27e8b6bd364820a7a15ecadb451fbe2
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-02 02:49:22 +0530

    Update day24.py

--- day24.py
+++ day24.py
@@ -1,17 +1,17 @@
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.sentiment import SentimentIntensityAnalyzer
+# import nltk
+# from nltk.tokenize import word_tokenize
+# from nltk.sentiment import SentimentIntensityAnalyzer
 
-nltk.download("punkt")
-nltk.download("vader_lexicon")
+# nltk.download("punkt")
+# nltk.download("vader_lexicon")
 
-text = "This is an example sentence. It showcases natural language processing."
+# text = "This is an example sentence. It showcases natural language processing."
 
-# Tokenization
-tokens = word_tokenize(text)
-print("Tokens:", tokens)
+# # Tokenization
+# tokens = word_tokenize(text)
+# print("Tokens:", tokens)
 
-# Sentiment Analysis
-sid = SentimentIntensityAnalyzer()
-sentiment_score = sid.polarity_scores(text)
-print("Sentiment Score:", sentiment_score)
+# # Sentiment Analysis
+# sid = SentimentIntensityAnalyzer()
+# sentiment_score = sid.polarity_scores(text)
+# print("Sentiment Score:", sentiment_score)
