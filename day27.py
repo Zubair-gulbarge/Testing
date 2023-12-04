618949afd1205d7962a5a16cb18c20d4ad5b63d5
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-05 00:52:59 +0530

    Update day27.py

--- day27.py
+++ day27.py
@@ -18,13 +18,13 @@
 # Description: Create a simple web application using the Flask framework.
 # Code:
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html", title="Flask Web App")
+# @app.route("/")
+# def home():
+#     return render_template("index.html", title="Flask Web App")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
