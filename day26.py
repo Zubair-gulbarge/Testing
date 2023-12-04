0ba5187fd7e477a4918a85c9754156369e3ae444
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-04 12:22:23 +0530

    Update day26.py

--- day26.py
+++ day26.py
@@ -7,25 +7,25 @@
 
 # asyncio.run(example_coroutine())
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-import numpy as np
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import mean_squared_error
+# import numpy as np
 
-# Generate sample data
-X = np.random.rand(100, 1)
-y = 2 * X + 1 + 0.1 * np.random.randn(100, 1)
+# # Generate sample data
+# X = np.random.rand(100, 1)
+# y = 2 * X + 1 + 0.1 * np.random.randn(100, 1)
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create and train a linear regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
+# # Create and train a linear regression model
+# model = LinearRegression()
+# model.fit(X_train, y_train)
 
-# Make predictions on the test set
-predictions = model.predict(X_test)
+# # Make predictions on the test set
+# predictions = model.predict(X_test)
 
-# Evaluate the model
-mse = mean_squared_error(y_test, predictions)
-print("Mean Squared Error:", mse)
+# # Evaluate the model
+# mse = mean_squared_error(y_test, predictions)
+# print("Mean Squared Error:", mse)
