926070147e37845faec7ebab514ce766b452f341
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-04 12:21:02 +0530

    Update day26.py

--- day26.py
+++ day26.py
@@ -1,8 +1,8 @@
-import asyncio
+# import asyncio
 
-async def example_coroutine():
-    print("Start Coroutine")
-    await asyncio.sleep(2)
-    print("End Coroutine")
+# async def example_coroutine():
+#     print("Start Coroutine")
+#     await asyncio.sleep(2)
+#     print("End Coroutine")
 
-asyncio.run(example_coroutine())
+# asyncio.run(example_coroutine())
