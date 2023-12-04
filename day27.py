1ff2d0fcd48889a782db742751dc1b87cba996ff
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-05 00:51:23 +0530

    Update day27.py

--- day27.py
+++ day27.py
@@ -3,12 +3,12 @@
 # Description: Use the Pandas library to perform data analysis on a given dataset.
 # Code:
 
-import pandas as pd
+# import pandas as pd
 
-# Create a DataFrame from a CSV file
-df = pd.read_csv("sample_data.csv")
+# # Create a DataFrame from a CSV file
+# df = pd.read_csv("sample_data.csv")
 
-# Perform data analysis tasks (e.g., describe, groupby, plot)
-print(df.describe())
-print(df.groupby("category")["value"].mean())
-df.plot(x="date", y="value", kind="line")
+# # Perform data analysis tasks (e.g., describe, groupby, plot)
+# print(df.describe())
+# print(df.groupby("category")["value"].mean())
+# df.plot(x="date", y="value", kind="line")
