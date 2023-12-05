8203470bd60d97f28aca132c9272240a10e747bc
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-06 00:39:49 +0530

    Update day 28.py

--- day28.py
+++ day28.py
@@ -46,3 +46,18 @@
 
 # Description: Implement a simple neural network using the TensorFlow library.
 # Code:
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Create a simple neural network
+model = Sequential()
+model.add(Dense(units=64, activation="relu", input_dim=100))
+model.add(Dense(units=1, activation="sigmoid"))
+
+# Compile the model
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# Train the model
+model.fit(x_train, y_train, epochs=10, batch_size=32)
