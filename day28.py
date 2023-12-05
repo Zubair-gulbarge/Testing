cfbccd7ba40ffd36bc30e173533f3193ef00083c
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-06 00:37:30 +0530

    Update day28.py

--- day28.py
+++ day28.py
@@ -3,18 +3,18 @@
 # Description: Create a graphical user interface (GUI) application using the Tkinter library.
 # Code:
 
-import tkinter as tk
+# import tkinter as tk
 
-def on_button_click():
-    label.config(text="Button Clicked!")
+# def on_button_click():
+#     label.config(text="Button Clicked!")
 
-app = tk.Tk()
-app.title("Tkinter GUI App")
+# app = tk.Tk()
+# app.title("Tkinter GUI App")
 
-button = tk.Button(app, text="Click me!", command=on_button_click)
-button.pack()
+# button = tk.Button(app, text="Click me!", command=on_button_click)
+# button.pack()
 
-label = tk.Label(app, text="Welcome to Tkinter!")
-label.pack()
+# label = tk.Label(app, text="Welcome to Tkinter!")
+# label.pack()
 
-app.mainloop()
+# app.mainloop()
