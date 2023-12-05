e8cb286af4f3bd11fc47f0500de4ccbff4d02b71
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-06 00:37:02 +0530

    Update day28.py

--- day28.py
+++ day28.py
@@ -2,3 +2,19 @@
 
 # Description: Create a graphical user interface (GUI) application using the Tkinter library.
 # Code:
+
+import tkinter as tk
+
+def on_button_click():
+    label.config(text="Button Clicked!")
+
+app = tk.Tk()
+app.title("Tkinter GUI App")
+
+button = tk.Button(app, text="Click me!", command=on_button_click)
+button.pack()
+
+label = tk.Label(app, text="Welcome to Tkinter!")
+label.pack()
+
+app.mainloop()
