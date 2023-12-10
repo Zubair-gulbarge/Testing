022af9521d85fe6040c383c051c70ac55644f095
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-10 12:59:41 +0530

    Update day30.py

--- day30.py
+++ day30.py
@@ -31,3 +31,14 @@ print(f"Mean Squared Error: {mse}")
 
 # Description: Create a simple web application using the Flask framework.
 # Code:
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", message="Welcome to Flask!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
