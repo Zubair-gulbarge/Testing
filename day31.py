e1f4bac4324f8b97a6452517ce7fb22c3f1a5a59
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-11 00:45:39 +0530

    Update day31.py

--- day31.py
+++ day31.py
@@ -2,3 +2,21 @@
 
 # Description: Implement asynchronous programming using the asyncio library.
 # Code:
+
+import asyncio
+
+async def task1():
+    print("Task 1 started")
+    await asyncio.sleep(2)
+    print("Task 1 completed")
+
+async def task2():
+    print("Task 2 started")
+    await asyncio.sleep(1)
+    print("Task 2 completed")
+
+async def main():
+    await asyncio.gather(task1(), task2())
+
+asyncio.run(main())
+
