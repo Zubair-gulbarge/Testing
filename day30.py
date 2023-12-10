12967f4b01568d96703d9f2c7860bd45d0d12977
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-10 13:00:38 +0530

    Update day30.py

--- day30.py
+++ day30.py
@@ -47,3 +47,33 @@ if __name__ == "__main__":
 
 # Description: Use the SQLAlchemy library to interact with a database.
 # Code:
+
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Define the database model
+Base = declarative_base()
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
+    username = Column(String(50))
+    email = Column(String(50))
+
+# Create an SQLite database in memory
+engine = create_engine("sqlite:///:memory:")
+
+# Create the table
+Base.metadata.create_all(engine)
+
+# Add data to the database
+Session = sessionmaker(bind=engine)
+session = Session()
+user1 = User(username="john_doe", email="john@example.com")
+session.add(user1)
+session.commit()
+
+# Query the database
+users = session.query(User).all()
+for user in users:
+    print(f"User ID: {user.id}, Username: {user.username}, Email: {user.email}")
