13003bfdd511eda2152842f464fff36504486024
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-12 01:08:09 +0530

    update day32.py

--- day32.py
+++ day32.py
@@ -3,12 +3,12 @@
 # Description: Use the requests library to interact with a public API.
 # Code:
 
-import requests
+# import requests
 
-# Make a request to a public API (e.g., JSONPlaceholder)
-url = "https://jsonplaceholder.typicode.com/posts/1"
-response = requests.get(url)
+# # Make a request to a public API (e.g., JSONPlaceholder)
+# url = "https://jsonplaceholder.typicode.com/posts/1"
+# response = requests.get(url)
 
-# Extract and print JSON data
-data = response.json()
-print(data)
+# # Extract and print JSON data
+# data = response.json()
+# print(data)
