566f4a69e4b1bb40721aa1707e83232d800f0440
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-14 00:25:48 +0530

    Update day33.py

--- day33.py
+++ day33.py
@@ -62,22 +62,22 @@ conn.close()
 # Description: Scrape data from a website using the requests and BeautifulSoup libraries.
 # Code:
 
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-url = 'https://example.com'
-response = requests.get(url)
+# url = 'https://example.com'
+# response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+# soup = BeautifulSoup(response.text, 'html.parser')
 
-# Extract data from the HTML
-title = soup.title.text
-paragraphs = soup.find_all('p')
+# # Extract data from the HTML
+# title = soup.title.text
+# paragraphs = soup.find_all('p')
 
-print(f'Title: {title}')
-print('Paragraphs:')
-for paragraph in paragraphs:
-    print(paragraph.text)
+# print(f'Title: {title}')
+# print('Paragraphs:')
+# for paragraph in paragraphs:
+#     print(paragraph.text)
 
 # Problem: Asynchronous Programming with asyncio
 
