f92fc80b596f6737a4d206d6c839ee74ee51ba98
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-13 06:02:04 +0530

    Update day33.py

--- day33.py
+++ day33.py
@@ -83,3 +83,23 @@ for paragraph in paragraphs:
 
 # Description: Explore asynchronous programming using the asyncio module.
 # Code:
+
+import asyncio
+
+async def print_numbers():
+    for i in range(5):
+        await asyncio.sleep(1)
+        print(i)
+
+async def print_letters():
+    for letter in 'ABCDE':
+        await asyncio.sleep(1)
+        print(letter)
+
+async def main():
+    task1 = asyncio.create_task(print_numbers())
+    task2 = asyncio.create_task(print_letters())
+
+    await asyncio.gather(task1, task2)
+
+asyncio.run(main())
