50fe185dffc3287e67677625b72f29c71d3eee84
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-15 06:41:08 +0530

    Update day34.py

--- day34.py
+++ day34.py
@@ -2,3 +2,25 @@
 
 # Description: Serialize and deserialize data using the JSON format.
 # Code:
+
+import json
+
+# Python dictionary
+data = {
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York'
+}
+
+# Serialize to JSON
+json_data = json.dumps(data, indent=2)
+
+# Print JSON
+print(json_data)
+
+# Deserialize JSON
+decoded_data = json.loads(json_data)
+
+# Print Python dictionary
+print(decoded_data)
+
