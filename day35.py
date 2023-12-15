84906268621a6f686f42693e40bab07ca738964c
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-16 03:10:23 +0530

    Update day35.py

--- day35.py
+++ day35.py
@@ -44,37 +44,37 @@
 # # Driver code 
 # main()
 
-def test_1(string =""): 
+# def test_1(string =""): 
 	
-	# initializing the substring 
-	substring = "" 
-	testList = [] 
-	initial = 0
+# 	# initializing the substring 
+# 	substring = "" 
+# 	testList = [] 
+# 	initial = 0
 	
-	for char in string: 
+# 	for char in string: 
 		
-		for i in range(initial, len(string)): 
-			substring+= string[i] 
+# 		for i in range(initial, len(string)): 
+# 			substring+= string[i] 
 			
-			# checking conditions 
-			if substring.count(string[i])>1: 
-				testList.append(substring[:-1]) 
-				initial+= 1
-				substring = "" 
-				break
-	maxi ="" 
+# 			# checking conditions 
+# 			if substring.count(string[i])>1: 
+# 				testList.append(substring[:-1]) 
+# 				initial+= 1
+# 				substring = "" 
+# 				break
+# 	maxi ="" 
 	
-	for word in testList: 
+# 	for word in testList: 
 		
-		if len(word)>len(maxi): 
-			maxi = word 
+# 		if len(word)>len(maxi): 
+# 			maxi = word 
 			
-	if len(maxi)<3: 
-		return "-1"
-	else: 
-		return maxi 
+# 	if len(maxi)<3: 
+# 		return "-1"
+# 	else: 
+# 		return maxi 
 	
-# Driver code 
-print(test_1("character")) 
-print(test_1("standfan")) 
-print(test_1("class"))
+# # Driver code 
+# print(test_1("character")) 
+# print(test_1("standfan")) 
+# print(test_1("class"))
