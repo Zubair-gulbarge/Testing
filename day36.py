e003ea8006aa207f2a8a75de6c3673d5800c32e8
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-17 02:11:24 +0530

    Update day36.py

--- day36.py
+++ day36.py
@@ -1,2 +1,19 @@
 # Description: Create a simple web scraper using BeautifulSoup and Requests to extract information from a website.
 # Code:
+
+import requests
+from bs4 import BeautifulSoup
+
+# Specify the URL to scrape
+url = 'https://example.com'
+
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Parse the HTML content with BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract information (example: titles)
+titles = soup.find_all('h2')
+for title in titles:
+    print(title.text)
