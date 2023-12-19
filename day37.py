4ce56d2c63f5b9a98b388ca75669f4c6874c8d6a
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-19 14:36:05 +0530

    Update day37.py

--- day37.py
+++ day37.py
@@ -1,25 +1,25 @@
 # Description: Implement a simple machine learning model using the Scikit-Learn library.
 # Code:
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score
 
-# Sample data (replace with your dataset)
-X, y = dataset_features, dataset_labels
+# # Sample data (replace with your dataset)
+# X, y = dataset_features, dataset_labels
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a Random Forest classifier
-clf = RandomForestClassifier()
+# # Create a Random Forest classifier
+# clf = RandomForestClassifier()
 
-# Train the model
-clf.fit(X_train, y_train)
+# # Train the model
+# clf.fit(X_train, y_train)
 
-# Make predictions on the test set
-predictions = clf.predict(X_test)
+# # Make predictions on the test set
+# predictions = clf.predict(X_test)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, predictions)
-print(f'Accuracy: {accuracy}')
+# # Evaluate the model
+# accuracy = accuracy_score(y_test, predictions)
+# print(f'Accuracy: {accuracy}')
