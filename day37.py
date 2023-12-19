a24451702efd5abc94b996447abb714fa32365df
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-19 14:41:53 +0530

    Update day37.py

--- day37.py
+++ day37.py
@@ -64,3 +64,19 @@
 
 # Description: Implement asynchronous web scraping using the aiohttp library.
 # Code:
+
+import aiohttp
+import asyncio
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+async def main():
+    url = 'https://example.com'
+    content = await fetch(url)
+    print(content)
+
+if __name__ == '__main__':
+    asyncio.run(main())
