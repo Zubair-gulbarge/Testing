2d772207187317f92dbd0931fb01c4468de2d886
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-19 14:37:46 +0530

    Update day37.py

--- day37.py
+++ day37.py
@@ -28,3 +28,18 @@
 
 # Description: Create visualizations using the Matplotlib library.
 # Code:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generate sample data
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+
+# Plot the sine function
+plt.plot(x, y, label='sin(x)')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.title('Sine Function')
+plt.legend()
+plt.show()
