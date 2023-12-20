2fce41bd78a411dc3a744ff19423940a74a18605
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-12-20 17:21:22 +0530

    Update day38.py

--- day38.py
+++ day38.py
@@ -34,3 +34,32 @@
 
 # Description: Implement a simple neural network using the TensorFlow library.
 # Code:
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Sample data (replace with your dataset)
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Build a simple neural network
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10)
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(f'Test accuracy: {test_acc}')
