164bedadfb7749884f6bfadba6397caeee830c10
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-26 18:57:02 +0530

    Update day39.py

--- day39.py
+++ day39.py
@@ -1,3 +1,12 @@
 # Problem: Asynchronous Programming with Asyncio
 # Description: Implement a simple asynchronous program using the asyncio library.
 # Code:
+
+import asyncio
+
+async def hello_world():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("World")
+
+asyncio.run(hello_world())
