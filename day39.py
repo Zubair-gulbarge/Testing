13baf1c4e9e9c4ab8b934de4992e19aec19a0ec5
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-26 19:00:16 +0530

    Update day39.py

--- day39.py
+++ day39.py
@@ -31,3 +31,34 @@
 # Problem: Working with Databases and SQLAlchemy
 # Description: Create a Python script to interact with a SQLite database using SQLAlchemy.
 # Code:
+
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Define the database model
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    age = Column(Integer)
+
+# Connect to the database
+engine = create_engine('sqlite:///:memory:')
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Add a new user
+new_user = User(name='John Doe', age=25)
+session.add(new_user)
+session.commit()
+
+# Query the database
+users = session.query(User).all()
+for user in users:
+    print(f'User: {user.name}, Age: {user.age}')
