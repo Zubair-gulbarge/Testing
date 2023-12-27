65c30370900e5282342fd62eb87a00044adfee37
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-27 14:29:46 +0530

    Update day40.py

--- day40.py
+++ day40.py
@@ -1,3 +1,25 @@
 # Problem: GUI Development with Tkinter
 # Description: Create a simple GUI application using the Tkinter library.
 # Code:
+
+import tkinter as tk
+
+def on_button_click():
+    label.config(text="Hello, " + entry.get())
+
+# Create the main window
+root = tk.Tk()
+root.title("Simple GUI App")
+
+# Create widgets
+label = tk.Label(root, text="Enter your name:")
+entry = tk.Entry(root)
+button = tk.Button(root, text="Say Hello", command=on_button_click)
+
+# Arrange widgets
+label.pack()
+entry.pack()
+button.pack()
+
+# Start the main loop
+root.mainloop()
