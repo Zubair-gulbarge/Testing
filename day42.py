3f849efc1f998126306437bc3e0b9f71785a11a8
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-30 01:39:35 +0530

    Update day42.py

--- day42.py
+++ day42.py
@@ -8,20 +8,20 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 
 # Generate sample data
-np.random.seed(42)
-X = 2 * np.random.rand(100, 1)
-y = 4 + 3 * X + np.random.randn(100, 1)
+# np.random.seed(42)
+# X = 2 * np.random.rand(100, 1)
+# y = 4 + 3 * X + np.random.randn(100, 1)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split the data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train a linear regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
+# # Train a linear regression model
+# model = LinearRegression()
+# model.fit(X_train, y_train)
 
-# Make predictions on the test set
-predictions = model.predict(X_test)
+# # Make predictions on the test set
+# predictions = model.predict(X_test)
 
-# Evaluate the model
-mse = mean_squared_error(y_test, predictions)
-print(f'Mean Squared Error: {mse}')
+# # Evaluate the model
+# mse = mean_squared_error(y_test, predictions)
+# print(f'Mean Squared Error: {mse}')
