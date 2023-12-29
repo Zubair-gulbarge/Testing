c12640cbb8e87f6c323eb0fe92b4b82fa245ec33
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-29 23:00:36 +0530

    Update day41.py

--- day41.py
+++ day41.py
@@ -1,12 +1,12 @@
-# Taking multiple integer inputs in a loop
-n = int(input("Enter the number of values: "))
-values = []
-for _ in range(n):
-    value = int(input("Enter a value: "))
-    values.append(value)
+# # Taking multiple integer inputs in a loop
+# n = int(input("Enter the number of values: "))
+# values = []
+# for _ in range(n):
+#     value = int(input("Enter a value: "))
+#     values.append(value)
 
-# Example: Input: 3, followed by three values
-print(values)
+# # Example: Input: 3, followed by three values
+# print(values)
 
 
 
