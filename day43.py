8651baa7588b72bdbf86a9a89b3636a72f5280ab
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-31 12:49:43 +0530

    Update day43.py

--- day43.py
+++ day43.py
@@ -43,17 +43,17 @@
 # Description: Write unit tests for a Python function using the unittest framework.
 # Code:
 
-import unittest
+# import unittest
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
-class TestAddFunction(unittest.TestCase):
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(2, 3), 5)
+# class TestAddFunction(unittest.TestCase):
+#     def test_add_positive_numbers(self):
+#         self.assertEqual(add(2, 3), 5)
 
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-2, -3), -5)
+#     def test_add_negative_numbers(self):
+#         self.assertEqual(add(-2, -3), -5)
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
