217c1016935909361b7e3d9cc5a778594d8b5ba3
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-31 12:45:01 +0530

    Update day43.py

--- day43.py
+++ day43.py
@@ -2,17 +2,18 @@
 # Description: Create a simple data visualization using the Matplotlib library.
 # Code:
 
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-# Generate sample data
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+# # Generate sample data
+# x = np.linspace(0, 10, 100)
+# y = np.sin(x)
 
-# Plot the data
-plt.plot(x, y, label='sin(x)')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Sinusoidal Function')
-plt.legend()
-plt.show()
+# # Plot the data
+# plt.plot(x, y, label='sin(x)')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Sinusoidal Function')
+# plt.legend()
+# plt.show()
+
