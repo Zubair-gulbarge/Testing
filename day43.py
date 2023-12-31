7886679dd83112c011a867b89537cb71fe9197fe
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2023-12-31 12:49:21 +0530

    Update day43.py

--- day43.py
+++ day43.py
@@ -42,3 +42,18 @@
 # Problem: Unit Testing with unittest
 # Description: Write unit tests for a Python function using the unittest framework.
 # Code:
+
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -3), -5)
+
+if __name__ == '__main__':
+    unittest.main()
