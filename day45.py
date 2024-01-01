f95805439be9ee70d802f733e88a7b9eada45991
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-02 03:41:52 +0530

    Update day45.py

--- day45.py
+++ day45.py
@@ -2,3 +2,11 @@
 # Description: Understand and use asynchronous programming with the asyncio library.
 # Code:
 
+import asyncio
+
+async def hello_world():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("World")
+
+asyncio.run(hello_world())
