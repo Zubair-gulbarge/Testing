1ac32cbbd4dfcbe5202f692b6b7c2f852dcc0187
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-04 14:14:29 +0530

    Update day47.py

--- day47.py
+++ day47.py
@@ -1,3 +1,10 @@
 # Problem: Working with APIs (Requests)
 # Description: Make API requests and process the JSON response using the Requests library.
 # Code:
+import requests
+
+# Make a GET request to a sample API
+response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+
+# Print the response content
+print(response.json())
