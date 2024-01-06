4d984fac10640e509804ac6a9335859742f54583
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-07 01:45:57 +0530

    Update day50.py

--- day50.py
+++ day50.py
@@ -7,3 +7,7 @@
 # class User(models.Model):
 #     name = models.CharField(max_length=100)
 #     age = models.IntegerField()
+
+# Problem: Django - Views and Templates
+# Description: Create views and templates for a Django web application.
+# Code (views.py):
