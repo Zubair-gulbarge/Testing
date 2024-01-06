bcc37c533a6d8fc5d426b2f4c82c7e26b7a4848f
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-06 13:53:59 +0530

    Update day49.py

--- day49.py
+++ day49.py
@@ -42,3 +42,7 @@
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+# Problem: Flask - Dynamic Routes and Templates
+# Description: Create dynamic routes and use templates in a Flask web application.
+# Code:
