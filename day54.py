fe7b43e1d8490494e35014a55e2da835a83cd022
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-11 07:41:43 +0530

    Update day54.py

--- day54.py
+++ day54.py
@@ -2,17 +2,17 @@
 # - Description: Use BeautifulSoup to scrape data from a website.
 # - Code:
 
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 
-# Make a request to the website
-response = requests.get('https://example.com')
+# # Make a request to the website
+# response = requests.get('https://example.com')
 
-# Parse the HTML content
-soup = BeautifulSoup(response.text, 'html.parser')
+# # Parse the HTML content
+# soup = BeautifulSoup(response.text, 'html.parser')
 
-# Extract information
-title = soup.title.text
-paragraphs = soup.find_all('p')
-for paragraph in paragraphs:
-    print(paragraph.text)
+# # Extract information
+# title = soup.title.text
+# paragraphs = soup.find_all('p')
+# for paragraph in paragraphs:
+#     print(paragraph.text)
