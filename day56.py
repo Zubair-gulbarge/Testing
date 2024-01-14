94a402dc57cc6ddb6bcf7d33a9d99d60488a2bed
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-14 19:23:25 +0530

    Update day56.py

--- day56.py
+++ day56.py
@@ -1,3 +1,13 @@
 # Problem: Asynchronous Programming with asyncio
 # - Description: Explore asynchronous programming using the asyncio library in Python.
 # - Code:
+
+import asyncio
+
+async def hello():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("World")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(hello())
