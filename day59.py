6a8cbdad04ab9c519b9044acc707becc4fcc266c
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-19 03:26:30 +0530

    Update day59.py

--- day59.py
+++ day59.py
@@ -2,3 +2,15 @@
 
 # Description: Implement the inorder traversal of a binary tree.
 # Code:
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.value)
+        inorder_traversal(root.right)
