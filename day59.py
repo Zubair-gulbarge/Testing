ded009e25d63dc4759c6d60203f817d3dc915e58
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-19 03:41:04 +0530

    Update day59.py

--- day59.py
+++ day59.py
@@ -20,21 +20,21 @@
 # Description: Implement a basic hash table.
 # Code:
 
-class HashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(size)]
+# class HashTable:
+#     def __init__(self, size):
+#         self.size = size
+#         self.table = [[] for _ in range(size)]
 
-    def hash_function(self, key):
-        return hash(key) % self.size
+#     def hash_function(self, key):
+#         return hash(key) % self.size
 
-    def insert(self, key, value):
-        index = self.hash_function(key)
-        self.table[index].append((key, value))
+#     def insert(self, key, value):
+#         index = self.hash_function(key)
+#         self.table[index].append((key, value))
 
-    def lookup(self, key):
-        index = self.hash_function(key)
-        for k, v in self.table[index]:
-            if k == key:
-                return v
-        return None
+#     def lookup(self, key):
+#         index = self.hash_function(key)
+#         for k, v in self.table[index]:
+#             if k == key:
+#                 return v
+#         return None
