23a076a1c49174f46d11de81f0f3731e8a478e4b
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-20 01:57:18 +0530

    Update day60.py

--- day60.py
+++ day60.py
@@ -3,11 +3,11 @@
 # Description: Implement the quick sort algorithm.
 # Code:
 
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[0]
-        left = [x for x in arr[1:] if x <= pivot]
-        right = [x for x in arr[1:] if x > pivot]
-        return quick_sort(left) + [pivot] + quick_sort(right)
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     else:
+#         pivot = arr[0]
+#         left = [x for x in arr[1:] if x <= pivot]
+#         right = [x for x in arr[1:] if x > pivot]
+#         return quick_sort(left) + [pivot] + quick_sort(right)
