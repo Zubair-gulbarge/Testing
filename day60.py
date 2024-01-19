9bff503fce80a9e067a6248deb0bc758bde119f4
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-20 01:58:55 +0530

    Update day60.py

--- day60.py
+++ day60.py
@@ -16,3 +16,21 @@
 
 # Description: Implement a stack data structure.
 # Code:
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
