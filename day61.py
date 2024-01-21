07accbd1df03289c2dd075bfa1f5d95d6a749000
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-21 20:29:51 +0530

    Update day61.py

--- day61.py
+++ day61.py
@@ -13,21 +13,21 @@
 
 # Description: Implement a basic linked list data structure.
 # Code:
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data=None):
+#         self.data = data
+#         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
+#     def append(self, data):
+#         new_node = Node(data)
+#         if not self.head:
+#             self.head = new_node
+#         else:
+#             current = self.head
+#             while current.next:
+#                 current = current.next
+#             current.next = new_node
