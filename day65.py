8fd02c942b6996144b193ec083d4c5c01abd7f3a
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-26 03:20:15 +0530

    Update day65.py

--- day65.py
+++ day65.py
@@ -80,8 +80,8 @@
 # Description: Implement the depth-first search algorithm on a binary tree.
 # Code:
 
-def dfs_binary_tree(node):
-    if node is not None:
-        print(node.data)
-        dfs_binary_tree(node.left)
-        dfs_binary_tree(node.right)
+# def dfs_binary_tree(node):
+#     if node is not None:
+#         print(node.data)
+#         dfs_binary_tree(node.left)
+#         dfs_binary_tree(node.right)
