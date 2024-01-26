052ec74e7c919a2bd5805bd8158c669ff470afc1
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-27 02:13:28 +0530

    Update day66.py

--- day66.py
+++ day66.py
@@ -2,19 +2,19 @@
 # Description: Implement the breadth-first search algorithm on a binary tree.
 # Code:
 
-def bfs_binary_tree(root):
-    if root is None:
-        return
+# def bfs_binary_tree(root):
+#     if root is None:
+#         return
     
-    queue = []
-    queue.append(root)
+#     queue = []
+#     queue.append(root)
     
-    while queue:
-        current_node = queue.pop(0)
-        print(current_node.data)
+#     while queue:
+#         current_node = queue.pop(0)
+#         print(current_node.data)
         
-        if current_node.left:
-            queue.append(current_node.left)
+#         if current_node.left:
+#             queue.append(current_node.left)
         
-        if current_node.right:
-            queue.append(current_node.right)
+#         if current_node.right:
+#             queue.append(current_node.right)
