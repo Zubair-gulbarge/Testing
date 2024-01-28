46a548aa58622ed5ecadb6b72244c2a6d7b3a81c
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-28 18:07:57 +0530

    Update day67.py

--- day67.py
+++ day67.py
@@ -1,3 +1,9 @@
 # Problem: In-order Traversal of a Binary Search Tree (BST)
 # Description: Implement the in-order traversal of a binary search tree.
 # Code:
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.data)
+        inorder_traversal(root.right)
