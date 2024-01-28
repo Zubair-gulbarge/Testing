93e700d1619e18d866078ac1ae429f1a7a35317f
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-28 18:10:27 +0530

    Update day67.py

--- day67.py
+++ day67.py
@@ -2,11 +2,11 @@
 # Description: Implement the in-order traversal of a binary search tree.
 # Code:
 
-def inorder_traversal(root):
-    if root:
-        inorder_traversal(root.left)
-        print(root.data)
-        inorder_traversal(root.right)
+# def inorder_traversal(root):
+#     if root:
+#         inorder_traversal(root.left)
+#         print(root.data)
+#         inorder_traversal(root.right)
 
 # Problem: Finding the Height of a Binary Tree
 # Description: Implement a function to find the height of a binary tree.
