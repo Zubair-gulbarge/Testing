e50330d8e7e5d0f012e49b7364f794a25b1f274c
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-30 04:32:24 +0530

    Update day68.py

--- day68.py
+++ day68.py
@@ -2,11 +2,11 @@
 # Description: Implement the depth-first search algorithm on a graph.
 # Code:
 
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    print(start)
-    for next_node in graph[start] - visited:
-        dfs(graph, next_node, visited)
-    return visited
+# def dfs(graph, start, visited=None):
+#     if visited is None:
+#         visited = set()
+#     visited.add(start)
+#     print(start)
+#     for next_node in graph[start] - visited:
+#         dfs(graph, next_node, visited)
+#     return visited
