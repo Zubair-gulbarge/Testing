17568a9e1b04603594f492691bf336e33ead1dc3
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-31 13:41:58 +0530

    Update day69.py

--- day69.py
+++ day69.py
@@ -18,3 +18,17 @@
 # Problem: Check for Bipartite Graph
 # Description: Determine whether a given graph is bipartite or not.
 # Code:
+
+def is_bipartite(graph, start):
+    color = {}
+    queue = [start]
+    color[start] = 0
+    while queue:
+        node = queue.pop(0)
+        for neighbor in graph[node]:
+            if neighbor not in color:
+                color[neighbor] = 1 - color[node]
+                queue.append(neighbor)
+            elif color[neighbor] == color[node]:
+                return False
+    return True
