679510425ad7adb3a220a136f6ce427903fd9295
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-01-31 13:41:05 +0530

    Update day69.py

--- day69.py
+++ day69.py
@@ -2,15 +2,15 @@
 # Description: Implement the breadth-first search algorithm on a graph.
 # Code:
 
-from collections import deque
-def bfs(graph, start):
-    visited = set()
-    queue = deque([start])
-    visited.add(start)
-    while queue:
-        node = queue.popleft()
-        print(node)
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append(neighbor)
+# from collections import deque
+# def bfs(graph, start):
+#     visited = set()
+#     queue = deque([start])
+#     visited.add(start)
+#     while queue:
+#         node = queue.popleft()
+#         print(node)
+#         for neighbor in graph[node]:
+#             if neighbor not in visited:
+#                 visited.add(neighbor)
+#                 queue.append(neighbor)
