9fcaae30cbf47f575d9a68ecbb1a9fc57b0ad434
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-04 07:24:49 +0530

    Update day72.py

--- day72.py
+++ day72.py
@@ -2,12 +2,12 @@
 # Description: Given an array of integers, find the length of the longest increasing subsequence.
 # Code:
 
-def length_of_lis(nums):
-    if not nums:
-        return 0
-    dp = [1] * len(nums)
-    for i in range(1, len(nums)):
-        for j in range(i):
-            if nums[i] > nums[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
-    return max(dp)
+# def length_of_lis(nums):
+#     if not nums:
+#         return 0
+#     dp = [1] * len(nums)
+#     for i in range(1, len(nums)):
+#         for j in range(i):
+#             if nums[i] > nums[j]:
+#                 dp[i] = max(dp[i], dp[j] + 1)
+#     return max(dp)
