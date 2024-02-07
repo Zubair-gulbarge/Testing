c43de5bd01ea3a35bcdd3c9bd12af901c2deaedf
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-08 02:01:00 +0530

    Update day73.py

--- day73.py
+++ day73.py
@@ -2,11 +2,11 @@
 # Description: Implement the depth-first search algorithm to traverse a graph.
 # Code:
 
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    for neighbor in graph[start]:
-        if neighbor not in visited:
-            dfs(graph, neighbor, visited)
-    return visited
+# def dfs(graph, start, visited=None):
+#     if visited is None:
+#         visited = set()
+#     visited.add(start)
+#     for neighbor in graph[start]:
+#         if neighbor not in visited:
+#             dfs(graph, neighbor, visited)
+#     return visited
