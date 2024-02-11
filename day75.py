da310d9ccf919272b175fe5502f64804fd048565
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-11 15:16:20 +0530

    Update day75.py

--- day75.py
+++ day75.py
@@ -39,10 +39,10 @@
 # Description: Implement the bubble sort algorithm to sort an array of integers.
 # Code:
 
-def bubble_sort(nums):
-    n = len(nums)
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
-            if nums[j] > nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
-    return nums
+# def bubble_sort(nums):
+#     n = len(nums)
+#     for i in range(n - 1):
+#         for j in range(0, n - i - 1):
+#             if nums[j] > nums[j + 1]:
+#                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+#     return nums
