2b58f05f97c072144b782c609fb333f41626754a
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-11 15:13:53 +0530

    Update day75.py

--- day75.py
+++ day75.py
@@ -18,11 +18,11 @@
 # Description: Implement the selection sort algorithm to sort an array of integers.
 # Code:
 
-def selection_sort(nums):
-    for i in range(len(nums)):
-        min_index = i
-        for j in range(i + 1, len(nums)):
-            if nums[j] < nums[min_index]:
-                min_index = j
-        nums[i], nums[min_index] = nums[min_index], nums[i]
-    return nums
+# def selection_sort(nums):
+#     for i in range(len(nums)):
+#         min_index = i
+#         for j in range(i + 1, len(nums)):
+#             if nums[j] < nums[min_index]:
+#                 min_index = j
+#         nums[i], nums[min_index] = nums[min_index], nums[i]
+#     return nums
