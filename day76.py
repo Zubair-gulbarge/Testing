f2076f9e0b110b187d6761cba492c16533b62016
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-12 23:36:46 +0530

    Update day76.py

--- day76.py
+++ day76.py
@@ -35,11 +35,11 @@
 # Description: Implement the quick sort algorithm to sort an array of integers.
 # Code:
 
-def quick_sort(nums):
-    if len(nums) <= 1:
-        return nums
-    pivot = nums[len(nums) // 2]
-    left = [x for x in nums if x < pivot]
-    middle = [x for x in nums if x == pivot]
-    right = [x for x in nums if x > pivot]
-    return quick_sort(left) + middle + quick_sort(right)
+# def quick_sort(nums):
+#     if len(nums) <= 1:
+#         return nums
+#     pivot = nums[len(nums) // 2]
+#     left = [x for x in nums if x < pivot]
+#     middle = [x for x in nums if x == pivot]
+#     right = [x for x in nums if x > pivot]
+#     return quick_sort(left) + middle + quick_sort(right)
