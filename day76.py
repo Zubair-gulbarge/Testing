d907146f3d325932772b44fe24f7792f992c5bd3
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-12 23:28:40 +0530

    Update day76.py

--- day76.py
+++ day76.py
@@ -2,12 +2,12 @@
 # Description: Implement the insertion sort algorithm to sort an array of integers.
 # Code:
 
-def insertion_sort(nums):
-    for i in range(1, len(nums)):
-        key = nums[i]
-        j = i - 1
-        while j >= 0 and key < nums[j]:
-            nums[j + 1] = nums[j]
-            j -= 1
-        nums[j + 1] = key
-    return nums
+# def insertion_sort(nums):
+#     for i in range(1, len(nums)):
+#         key = nums[i]
+#         j = i - 1
+#         while j >= 0 and key < nums[j]:
+#             nums[j + 1] = nums[j]
+#             j -= 1
+#         nums[j + 1] = key
+#     return nums
