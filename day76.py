3e6821a1cec2afcdada5fa5edffc67bf1910609a
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-12 23:38:58 +0530

    Update day76.py

--- day76.py
+++ day76.py
@@ -47,3 +47,18 @@
 # Problem: Find All Anagrams in a String
 # Description: Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 # Code:
+
+from collections import Counter
+
+def find_anagrams(s, p):
+    result = []
+    p_counter = Counter(p)
+    s_counter = Counter(s[:len(p) - 1])
+    for i in range(len(p) - 1, len(s)):
+        s_counter[s[i]] += 1
+        if s_counter == p_counter:
+            result.append(i - len(p) + 1)
+        s_counter[s[i - len(p) + 1]] -= 1
+        if s_counter[s[i - len(p) + 1]] == 0:
+            del s_counter[s[i - len(p) + 1]]
+    return result
