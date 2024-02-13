cc2f104c57534a45072f6462af2dadd35ddaa414
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-13 14:28:24 +0530

    Update day77.py

--- day77.py
+++ day77.py
@@ -43,8 +43,8 @@
 # Description: Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
 # Code:
 
-def missing_number(nums):
-    n = len(nums)
-    expected_sum = n * (n + 1) // 2
-    actual_sum = sum(nums)
-    return expected_sum - actual_sum
+# def missing_number(nums):
+#     n = len(nums)
+#     expected_sum = n * (n + 1) // 2
+#     actual_sum = sum(nums)
+#     return expected_sum - actual_sum
