b1e9adef042f7afec33f5cc8a90798a18cf8bd62
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-14 00:13:57 +0530

    Update day77.py

--- day77.py
+++ day77.py
@@ -69,19 +69,19 @@
 # Description: Reverse a singly linked list.
 # Code:
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-def reverse_linked_list(head):
-    prev, curr = None, head
-    while curr:
-        next_node = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next_node
-    return prev
+# def reverse_linked_list(head):
+#     prev, curr = None, head
+#     while curr:
+#         next_node = curr.next
+#         curr.next = prev
+#         prev = curr
+#         curr = next_node
+#     return prev
 
 # Problem: Check if a Binary Tree is Symmetric
 # Description: Given a binary tree, check whether it is a mirror of itself.
