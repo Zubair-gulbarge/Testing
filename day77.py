3499fe1c39466c84395a55e4cc8e23ff77d5a0be
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-13 14:33:26 +0530

    Update day77.py

--- day77.py
+++ day77.py
@@ -68,3 +68,18 @@
 # Problem: Reverse a Linked List
 # Description: Reverse a singly linked list.
 # Code:
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_linked_list(head):
+    prev, curr = None, head
+    while curr:
+        next_node = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_node
+    return prev
+
