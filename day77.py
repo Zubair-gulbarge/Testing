45b7eebf1103f6522dfcb39d57757a20bf5c4249
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-14 00:13:20 +0530

    Update day77.py

--- day77.py
+++ day77.py
@@ -87,20 +87,20 @@ def reverse_linked_list(head):
 # Description: Given a binary tree, check whether it is a mirror of itself.
 # Code:
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-def is_symmetric(root):
-    if not root:
-        return True
-    return is_mirror(root.left, root.right)
+# def is_symmetric(root):
+#     if not root:
+#         return True
+#     return is_mirror(root.left, root.right)
 
-def is_mirror(left, right):
-    if not left and not right:
-        return True
-    if not left or not right or left.val != right.val:
-        return False
-    return is_mirror(left.left, right.right) and is_mirror(left.right, right.left)
+# def is_mirror(left, right):
+#     if not left and not right:
+#         return True
+#     if not left or not right or left.val != right.val:
+#         return False
+#     return is_mirror(left.left, right.right) and is_mirror(left.right, right.left)
