5457cec82ac7ed021f81c97195e579e432a6e042
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-13 14:21:38 +0530

    Update day77.py

--- day77.py
+++ day77.py
@@ -1,14 +1,14 @@
-def max_subarray_sum(arr):
-    max_sum = float('-inf')
-    current_sum = 0
+# def max_subarray_sum(arr):
+#     max_sum = float('-inf')
+#     current_sum = 0
     
-    for num in arr:
-        current_sum = max(num, current_sum + num)
-        max_sum = max(max_sum, current_sum)
+#     for num in arr:
+#         current_sum = max(num, current_sum + num)
+#         max_sum = max(max_sum, current_sum)
     
-    return max_sum
+#     return max_sum
 
-# Example usage
-input_list = [1, -2, 3, 4, -1, 2, 1, -5, 4]
-result = max_subarray_sum(input_list)
-print("Maximum subarray sum:", result)
+# # Example usage
+# input_list = [1, -2, 3, 4, -1, 2, 1, -5, 4]
+# result = max_subarray_sum(input_list)
+# print("Maximum subarray sum:", result)
