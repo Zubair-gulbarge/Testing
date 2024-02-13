c2cc6bea4de7472688f3c81fb63a915aa33a74a9
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-13 14:32:01 +0530

    Update day77.py

--- day77.py
+++ day77.py
@@ -53,14 +53,14 @@
 # Description: Implement the binary search algorithm to find the index of a target element in a sorted array.
 # Code:
 
-def binary_search(nums, target):
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = left + (right - left) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
+# def binary_search(nums, target):
+#     left, right = 0, len(nums) - 1
+#     while left <= right:
+#         mid = left + (right - left) // 2
+#         if nums[mid] == target:
+#             return mid
+#         elif nums[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return -1
