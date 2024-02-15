4e05488f2ce85435eefda909017db2ffa6456f7d
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-15 17:12:05 +0530

    Update day79.py

--- day79.py
+++ day79.py
@@ -2,8 +2,8 @@
 # Description: Given a list of integers from 1 to n with one missing number, find the missing number.
 # Code:
 
-def find_missing_number(nums):
-    n = len(nums) + 1
-    expected_sum = n * (n + 1) // 2
-    actual_sum = sum(nums)
-    return expected_sum - actual_sum
+# def find_missing_number(nums):
+#     n = len(nums) + 1
+#     expected_sum = n * (n + 1) // 2
+#     actual_sum = sum(nums)
+#     return expected_sum - actual_sum
