e7740fc0ee70e8155ef89f49c1e8be1cb7e8ac41
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-15 17:04:39 +0530

    Update day78.py

--- day78.py
+++ day78.py
@@ -57,15 +57,15 @@ def power(x, n):
 # Description: Given an array of strings, find the longest common prefix amongst them.
 # Code:
 
-def longest_common_prefix(strs):
-    if not strs:
-        return ""
-    min_len = min(len(s) for s in strs)
-    prefix = ""
-    for i in range(min_len):
-        char = strs[0][i]
-        if all(s[i] == char for s in strs):
-            prefix += char
-        else:
-            break
-    return prefix
+# def longest_common_prefix(strs):
+#     if not strs:
+#         return ""
+#     min_len = min(len(s) for s in strs)
+#     prefix = ""
+#     for i in range(min_len):
+#         char = strs[0][i]
+#         if all(s[i] == char for s in strs):
+#             prefix += char
+#         else:
+#             break
+#     return prefix
