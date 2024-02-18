2bf342230bafdb04ee873812bcc8666b939bfae0
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-18 15:19:50 +0530

    Update day81.py

--- day81.py
+++ day81.py
@@ -2,8 +2,8 @@
 # Description: Generate the Fibonacci sequence up to a specified number of terms.
 # Code:
 
-def fibonacci(n):
-    fib = [0, 1]
-    while len(fib) < n:
-        fib.append(fib[-1] + fib[-2])
-    return fib[:n]
+# def fibonacci(n):
+#     fib = [0, 1]
+#     while len(fib) < n:
+#         fib.append(fib[-1] + fib[-2])
+#     return fib[:n]
