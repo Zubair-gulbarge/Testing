fb9e016c56432823b0b524e737fbbadcf4de455f
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-18 15:19:31 +0530

    Update day81.py

--- day81.py
+++ day81.py
@@ -1,3 +1,9 @@
 # Problem: Calculate the Fibonacci Sequence
 # Description: Generate the Fibonacci sequence up to a specified number of terms.
 # Code:
+
+def fibonacci(n):
+    fib = [0, 1]
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+    return fib[:n]
