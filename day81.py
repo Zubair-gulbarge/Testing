14ca059b02b6f68b58ef79ee2b650bede93b03ef
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-18 15:20:43 +0530

    Update day81.py

--- day81.py
+++ day81.py
@@ -11,3 +11,10 @@
 # Problem: Find the Maximum Subarray Sum
 # Description: Find the contiguous subarray within a one-dimensional array of numbers that has the largest sum.
 # Code:
+
+def max_subarray_sum(nums):
+    max_sum = curr_sum = nums[0]
+    for num in nums[1:]:
+        curr_sum = max(num, curr_sum + num)
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
