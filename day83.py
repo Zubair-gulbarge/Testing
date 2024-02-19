f6197105150a9bc9c0b6b2e56d0835e2096c586c
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-20 01:45:30 +0530

    Update day83.py

--- day83.py
+++ day83.py
@@ -26,3 +26,12 @@
 # Problem: Find the Longest Palindromic Substring
 # Description: Given a string s, find the longest palindromic substring in s.
 # Code:
+
+def longest_palindrome(s):
+    longest = ''
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            substring = s[i:j+1]
+            if substring == substring[::-1] and len(substring) > len(longest):
+                longest = substring
+    return longest
