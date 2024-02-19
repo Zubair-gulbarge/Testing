54eb2472ccd6796b2aa1adf6f4e0ca3631910a40
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-20 01:47:32 +0530

    Update day83.py

--- day83.py
+++ day83.py
@@ -40,19 +40,19 @@
 # Description: Merge two sorted linked lists and return it as a new sorted list.
 # Code:
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-def merge_two_lists(l1, l2):
-    if not l1:
-        return l2
-    if not l2:
-        return l1
-    if l1.val < l2.val:
-        l1.next = merge_two_lists(l1.next, l2)
-        return l1
-    else:
-        l2.next = merge_two_lists(l1, l2.next)
-        return l2
+# def merge_two_lists(l1, l2):
+#     if not l1:
+#         return l2
+#     if not l2:
+#         return l1
+#     if l1.val < l2.val:
+#         l1.next = merge_two_lists(l1.next, l2)
+#         return l1
+#     else:
+#         l2.next = merge_two_lists(l1, l2.next)
+#         return l2
