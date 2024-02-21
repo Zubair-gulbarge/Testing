c6a2e7519001e3ea16f49bfd125ee1796b143ef3
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-21 20:04:16 +0530

    Update day84.py

--- day84.py
+++ day84.py
@@ -2,11 +2,11 @@
 # Description: Reverse a singly linked list.
 # Code:
 
-def reverse_linked_list(head):
-    prev = None
-    while head:
-        temp = head.next
-        head.next = prev
-        prev = head
-        head = temp
-    return prev
+# def reverse_linked_list(head):
+#     prev = None
+#     while head:
+#         temp = head.next
+#         head.next = prev
+#         prev = head
+#         head = temp
+#     return prev
