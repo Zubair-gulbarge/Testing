6570bb308a7f24b7a70fb383e09a19268273bc1d
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-21 20:07:36 +0530

    Update day84.py

--- day84.py
+++ day84.py
@@ -48,33 +48,33 @@
 # Description: Implement a queue data structure using a linked list.
 # Code:
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
+# class Queue:
+#     def __init__(self):
+#         self.front = None
+#         self.rear = None
     
-    def enqueue(self, data):
-        new_node = Node(data)
-        if not self.rear:
-            self.front = new_node
-            self.rear = new_node
-        else:
-            self.rear.next = new_node
-            self.rear = new_node
+#     def enqueue(self, data):
+#         new_node = Node(data)
+#         if not self.rear:
+#             self.front = new_node
+#             self.rear = new_node
+#         else:
+#             self.rear.next = new_node
+#             self.rear = new_node
     
-    def dequeue(self):
-        if self.is_empty():
-            return None
-        data = self.front.data
-        self.front = self.front.next
-        if not self.front:
-            self.rear = None
-        return data
+#     def dequeue(self):
+#         if self.is_empty():
+#             return None
+#         data = self.front.data
+#         self.front = self.front.next
+#         if not self.front:
+#             self.rear = None
+#         return data
     
-    def is_empty(self):
-        return self.front is None
+#     def is_empty(self):
+#         return self.front is None
