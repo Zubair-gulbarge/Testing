8cb768fc38e63913a0ac16c0a906d4ab3b723d82
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-26 15:22:17 +0530

    Update day87.py

--- day87.py
+++ day87.py
@@ -33,10 +33,10 @@
 # Description: You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 # Code:
 
-def climb_stairs(n):
-    if n <= 2:
-        return n
-    prev, curr = 1, 2
-    for _ in range(3, n + 1):
-        prev, curr = curr, prev + curr
-    return curr
+# def climb_stairs(n):
+#     if n <= 2:
+#         return n
+#     prev, curr = 1, 2
+#     for _ in range(3, n + 1):
+#         prev, curr = curr, prev + curr
+#     return curr
