7406d376e67e14db3d00b74b1ca29ba6779d7cc3
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-26 15:19:29 +0530

    Update day87.py

--- day87.py
+++ day87.py
@@ -2,13 +2,13 @@
 # Description: Given a 32-bit signed integer, reverse digits of an integer.
 # Code:
 
-def reverse_integer(x):
-    sign = -1 if x < 0 else 1
-    x *= sign
-    reversed_int = 0
-    while x:
-        reversed_int = reversed_int * 10 + x % 10
-        x //= 10
-    if reversed_int > 2**31 - 1:
-        return 0
-    return reversed_int * sign
+# def reverse_integer(x):
+#     sign = -1 if x < 0 else 1
+#     x *= sign
+#     reversed_int = 0
+#     while x:
+#         reversed_int = reversed_int * 10 + x % 10
+#         x //= 10
+#     if reversed_int > 2**31 - 1:
+#         return 0
+#     return reversed_int * sign
