683ee42d2a3ae74a78bc352f8b9db5a6221c45e0
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-28 13:34:27 +0530

    Update day89.py

--- day89.py
+++ day89.py
@@ -1,3 +1,10 @@
 # Problem: Reverse Integer
 # Description: Given a 32-bit signed integer, reverse digits of an integer.
 # Code:
+
+def reverse_integer(x):
+    if x >= 0:
+        rev = int(str(x)[::-1])
+    else:
+        rev = -int(str(-x)[::-1])
+    return rev if -(2**31) <= rev <= (2**31 - 1) else 0
