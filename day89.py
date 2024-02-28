08d6974163370594972a79c5b044d0fd96443475
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-28 13:38:07 +0530

    Update day89.py

--- day89.py
+++ day89.py
@@ -22,14 +22,14 @@
 # Description: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 # Code:
 
-def is_valid(s):
-    stack = []
-    mapping = {')': '(', '}': '{', ']': '['}
-    for char in s:
-        if char in mapping:
-            top_element = stack.pop() if stack else '#'
-            if mapping[char] != top_element:
-                return False
-        else:
-            stack.append(char)
-    return not stack
+# def is_valid(s):
+#     stack = []
+#     mapping = {')': '(', '}': '{', ']': '['}
+#     for char in s:
+#         if char in mapping:
+#             top_element = stack.pop() if stack else '#'
+#             if mapping[char] != top_element:
+#                 return False
+#         else:
+#             stack.append(char)
+#     return not stack
