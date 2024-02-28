d71c13b90835500da19a5e85ff1fa6290b11ccf6
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-28 13:34:45 +0530

    Update day89.py

--- day89.py
+++ day89.py
@@ -2,9 +2,9 @@
 # Description: Given a 32-bit signed integer, reverse digits of an integer.
 # Code:
 
-def reverse_integer(x):
-    if x >= 0:
-        rev = int(str(x)[::-1])
-    else:
-        rev = -int(str(-x)[::-1])
-    return rev if -(2**31) <= rev <= (2**31 - 1) else 0
+# def reverse_integer(x):
+#     if x >= 0:
+#         rev = int(str(x)[::-1])
+#     else:
+#         rev = -int(str(-x)[::-1])
+#     return rev if -(2**31) <= rev <= (2**31 - 1) else 0
