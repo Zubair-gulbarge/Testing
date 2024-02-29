180d1a9389b95078f7d95948a938e67aec1b6ea3
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-02-29 14:10:00 +0530

    Update day90.py

--- day90.py
+++ day90.py
@@ -1,3 +1,12 @@
 # Problem: Two Sum
 # Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # Code:
+
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
