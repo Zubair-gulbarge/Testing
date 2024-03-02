309ea26761cbfe75b93c46cc07445ac059aa7777
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-02 17:46:08 +0530

    Update day91.py

--- day91.py
+++ day91.py
@@ -27,3 +27,10 @@
 # Problem: Implement strStr()
 # Description: Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 # Code:
+
+def str_str(haystack, needle):
+    if not needle:
+        return 0
+    if needle not in haystack:
+        return -1
+    return haystack.index(needle)
