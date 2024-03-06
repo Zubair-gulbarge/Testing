19db449098471ac9eb6732ced985bdc1cebc336e
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-07 03:35:14 +0530

    Update day93.py

--- day93.py
+++ day93.py
@@ -29,11 +29,11 @@
 # Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # Code:
 
-def two_sum(nums, target):
-    num_dict = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_dict:
-            return [num_dict[complement], i]
-        num_dict[num] = i
-    return None
+# def two_sum(nums, target):
+#     num_dict = {}
+#     for i, num in enumerate(nums):
+#         complement = target - num
+#         if complement in num_dict:
+#             return [num_dict[complement], i]
+#         num_dict[num] = i
+#     return None
