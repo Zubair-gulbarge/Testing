23b7a2d8ef3ef552e4efdb3d001ca26c23ba0a86
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-06 14:29:36 +0530

    Update day92.py

--- day92.py
+++ day92.py
@@ -2,12 +2,12 @@
 # Description: Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
 # Code:
 
-def longest_common_prefix(strs):
-    if not strs:
-        return ""
-    min_str = min(strs, key=len)
-    for i, char in enumerate(min_str):
-        for string in strs:
-            if string[i] != char:
-                return min_str[:i]
-    return min_str
+# def longest_common_prefix(strs):
+#     if not strs:
+#         return ""
+#     min_str = min(strs, key=len)
+#     for i, char in enumerate(min_str):
+#         for string in strs:
+#             if string[i] != char:
+#                 return min_str[:i]
+#     return min_str
