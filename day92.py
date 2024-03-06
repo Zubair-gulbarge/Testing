b611eae3e4f2576ec57f9337867ca2912bd4b055
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-06 14:30:28 +0530

    Update day92.py

--- day92.py
+++ day92.py
@@ -15,3 +15,20 @@
 # Problem: Merge Two Sorted Lists
 # Description: Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
 # Code:
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_two_lists(l1, l2):
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+    if l1.val < l2.val:
+        l1.next = merge_two_lists(l1.next, l2)
+        return l1
+    else:
+        l2.next = merge_two_lists(l1, l2.next)
+        return l2
