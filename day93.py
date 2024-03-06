915c193bf629a0b8a8403b19e7af621e08e5317a
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-07 03:32:29 +0530

    Update day93.py

--- day93.py
+++ day93.py
@@ -2,15 +2,15 @@
 # Description: Given a 32-bit signed integer, reverse digits of an integer.
 # Code:
 
-def reverse_integer(x):
-    sign = 1 if x >= 0 else -1
-    x = abs(x)
-    rev = 0
-    while x != 0:
-        pop = x % 10
-        x //= 10
-        rev = rev * 10 + pop
-    rev *= sign
-    if rev < -2**31 or rev > 2**31 - 1:
-        return 0
-    return rev
+# def reverse_integer(x):
+#     sign = 1 if x >= 0 else -1
+#     x = abs(x)
+#     rev = 0
+#     while x != 0:
+#         pop = x % 10
+#         x //= 10
+#         rev = rev * 10 + pop
+#     rev *= sign
+#     if rev < -2**31 or rev > 2**31 - 1:
+#         return 0
+#     return rev
