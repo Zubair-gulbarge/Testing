a63f50dba62c60fccc0f9fbcc31a3bd560a766e9
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-09 16:20:42 +0530

    Update day94.py

--- day94.py
+++ day94.py
@@ -27,3 +27,7 @@
 #         else:
 #             stack.append(char)
 #     return not stack
+
+# Problem: Container With Most Water
+# Description: Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+# Code:
