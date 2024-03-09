541931743533a04db7e6ec4d700df7e382d4b2e7
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-09 16:18:06 +0530

    Update day94.py

--- day94.py
+++ day94.py
@@ -1,3 +1,13 @@
 # Problem: Longest Common Prefix
 # Description: Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
 # Code:
+
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    min_str = min(strs, key=len)
+    for i, char in enumerate(min_str):
+        for other in strs:
+            if other[i] != char:
+                return min_str[:i]
+    return min_str
