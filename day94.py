0843ea76cc3390b06e24be9de9e90d35a3b7ee36
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-09 16:21:26 +0530

    Update day94.py

--- day94.py
+++ day94.py
@@ -32,13 +32,13 @@
 # Description: Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
 # Code:
 
-def max_area(height):
-    max_area = 0
-    left, right = 0, len(height) - 1
-    while left < right:
-        max_area = max(max_area, min(height[left], height[right]) * (right - left))
-        if height[left] < height[right]:
-            left += 1
-        else:
-            right -= 1
-    return max_area
+# def max_area(height):
+#     max_area = 0
+#     left, right = 0, len(height) - 1
+#     while left < right:
+#         max_area = max(max_area, min(height[left], height[right]) * (right - left))
+#         if height[left] < height[right]:
+#             left += 1
+#         else:
+#             right -= 1
+#     return max_area
