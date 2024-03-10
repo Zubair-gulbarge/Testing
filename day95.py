032a8c82adf89fb43f7db1a9573e0232d1dea533
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-03-10 14:29:16 +0530

    Update day95.py

--- day95.py
+++ day95.py
@@ -2,14 +2,14 @@
 # Description: Given a collection of intervals, merge all overlapping intervals.
 # Code:
 
-def merge(intervals):
-    if not intervals:
-        return []
-    intervals.sort(key=lambda x: x[0])
-    merged = [intervals[0]]
-    for interval in intervals[1:]:
-        if interval[0] <= merged[-1][1]:
-            merged[-1][1] = max(merged[-1][1], interval[1])
-        else:
-            merged.append(interval)
-    return merged
+# def merge(intervals):
+#     if not intervals:
+#         return []
+#     intervals.sort(key=lambda x: x[0])
+#     merged = [intervals[0]]
+#     for interval in intervals[1:]:
+#         if interval[0] <= merged[-1][1]:
+#             merged[-1][1] = max(merged[-1][1], interval[1])
+#         else:
+#             merged.append(interval)
+#     return merged
